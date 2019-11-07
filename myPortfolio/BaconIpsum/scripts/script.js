b7384dacd913ef07c0df071821902034f6bcf0fd
carl-larson <clarson08@gmail.com>
2019-11-06 19:18:37 -0600

    trying to add js to change color of Bacon pages

---
+++ myPortfolio/BaconIpsum/scripts/script.js
@@ -0,0 +1,7 @@
+var docTitle = document.title;
+
+function showDocTitle(){console.log(docTitle)}
+
+if (docTitle == Bacon Article 1 ){
+    document.body.style.backgroundColor = "green";
+}
---
+++ myPortfolio/scripts/ticTacToeScript.js
@@ -0,0 +1,15 @@
+
+
+var gameMarker = "x"
+
+function changeMarkerToX(){
+    console.log("The x button was clicked!")
+}
+
+function changeMarkerToO(){
+    console.log("The o button was clicked!")
+}
+
+function placeMark(){ 
+    console.log("The x button was clicked!")
+}
