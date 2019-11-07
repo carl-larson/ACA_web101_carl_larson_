1fa8337b290936a307a5d2b2b634751e50b5bdb7
carl-larson <clarson08@gmail.com>
2019-11-07 15:05:54 -0600

    manually colored all article pages. link to about page is hidden in google plus icon for now

--- myPortfolio/BaconIpsum/scripts/script.js
+++ myPortfolio/BaconIpsum/scripts/script.js
@@ -1,7 +1,17 @@
+// var docTitle = document.title;
+
+// function showDocTitle(){console.log(document.title)}
+
+// if (docTitle == "Bacon Article 1"){
+//     document.body.style.backgroundColor = "green";
+//     console.log("check");
+// }
+
 var docTitle = document.title;
+var lookFor1 = docTitle.innerHTML.indexOf("1");
 
-function showDocTitle(){console.log(docTitle)}
+function showDocTitle(){console.log(lookFor1)}
 
-if (docTitle == Bacon Article 1 ){
+if (lookFor1 != -1){
     document.body.style.backgroundColor = "green";
 }
