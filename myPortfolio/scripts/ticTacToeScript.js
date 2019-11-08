98c4ead513972f610efd9d4b43decaa0fd9cec3f
carl-larson <clarson08@gmail.com>
2019-11-07 21:17:12 -0600

    Success with TicTacToe

--- myPortfolio/scripts/ticTacToeScript.js
+++ myPortfolio/scripts/ticTacToeScript.js
@@ -1,28 +1,32 @@
 
-
-// var gameMarker = "x";
-var gameMarker;
-function changeMarkerToX(){
-    console.log("The x button was clicked!")
-    gameMarker = "x";
-    console.log(gameMarker)
+let gameMarker = "X";
+// let turn = 
+function changeMarkerToX(turn){
+    console.log("The x button was clicked!");
+    gameMarker = "X";
+    document.getElementById(turn).innerHTML = gameMarker;
 }
 
-function changeMarkerToO(){
-    console.log("The o button was clicked!")
-    gameMarker = "o";
-    console.log(gameMarker)
+function changeMarkerToO(turn){
+    console.log("The o button was clicked!");
+    gameMarker = "O";
+    document.getElementById(turn).innerHTML = gameMarker;
 }
 
 
-function placeMarkxA(gameMarker){
-    console.log(gameMarker);
-    if(gameMarker === "x"){
-    console.log("The marker is X");
-    document.getElementbyId("xA").innerHTML = "X";
+function placeMark(mySquare){
+    document.getElementById(mySquare).innerHTML = gameMarker;
+    if (gameMarker == "X"){
+        changeMarkerToO('turnName');
+    }else{
+        changeMarkerToX('turnName');
+    }
 }
-    else{
-    console.log("The marker is o");
-    document.getElementbyId("xA").innerHTML = "X";
-}
-}
+
+// function nextTurnO(){
+//     gameMarker = "O";
+//     changeMarkerToO;
+// }
+// function nextTurnX(){
+//     gameMarker = "X";
+// }
