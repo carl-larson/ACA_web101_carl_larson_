c49f4fe057b7359919d56856e53e876bfdf0e9d6
carl-larson <clarson08@gmail.com>
2019-11-07 19:31:17 -0600

    messing with tictactoe

--- myPortfolio/scripts/ticTacToeScript.js
+++ myPortfolio/scripts/ticTacToeScript.js
@@ -1,15 +1,28 @@
 
 
-var gameMarker = "x"
-
+// var gameMarker = "x";
+var gameMarker;
 function changeMarkerToX(){
     console.log("The x button was clicked!")
+    gameMarker = "x";
+    console.log(gameMarker)
 }
 
 function changeMarkerToO(){
     console.log("The o button was clicked!")
+    gameMarker = "o";
+    console.log(gameMarker)
 }
 
-function placeMark(){ 
-    console.log("The x button was clicked!")
+
+function placeMarkxA(gameMarker){
+    console.log(gameMarker);
+    if(gameMarker === "x"){
+    console.log("The marker is X");
+    document.getElementbyId("xA").innerHTML = "X";
+}
+    else{
+    console.log("The marker is o");
+    document.getElementbyId("xA").innerHTML = "X";
+}
 }
