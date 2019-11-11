5a25ffaf14c47de1ce554cb726e118d0867c7a5c
carl-larson <clarson08@gmail.com>
2019-11-11 17:57:33 -0600

    added geolocation API to dashboard

--- myPortfolio/scripts/dashboard.js
+++ myPortfolio/scripts/dashboard.js
@@ -15,3 +15,17 @@ $(function() {
     $this.remove();
     });
 });
+    var myLocation = document.getElementById("my-coordinates")
+
+    function getLocation(){
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(displayPosition)
+        }else{
+            myLocation.innerHTML = "Not Available"
+        }
+    }
+    function displayPosition(position){
+        myLocation.innerHTML= "Longitude: " + position.coords.longitude + " Latitude:" + position.coords.latitude
+    }
+
+
