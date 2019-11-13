74bae431b75ee32627ddee73fe652f153aa748ca
carl-larson <clarson08@gmail.com>
2019-11-12 20:49:02 -0600

    made the clock and date function on Dashboard

--- myPortfolio/scripts/dashboard.js
+++ myPortfolio/scripts/dashboard.js
@@ -1,3 +1,5 @@
+
+// Add toDo List Functionality
 $(function() {
 
     let $list = $('#ultodo');
@@ -15,6 +17,8 @@ $(function() {
     $this.remove();
     });
 });
+
+// Add GeoLocation
     var myLocation = document.getElementById("my-coordinates")
 
     function getLocation(){
@@ -27,5 +31,34 @@ $(function() {
     function displayPosition(position){
         myLocation.innerHTML= "Longitude: " + position.coords.longitude + " Latitude:" + position.coords.latitude
     }
+
+// Build Functioning Clock
+    var showTime = document.getElementById('time');
+    var showDate = document.getElementById('showDate');
+
+function time() {
+    var today = new Date();
+    var s =today.getSeconds();
+    var m =today.getMinutes();
+    var h =today.getHours();
+    if (s<10) {
+        s= '0' + s;
+    }
+    if (h<10) {
+        h= '0' + h;
+    }
+    if (m<10) {
+        m= '0' + m;
+    }
+    var time = h + ":" + m + ":" + s;
+    showTime.innerHTML=time;
+
+}
+setInterval(time, 1000);
+
+var today = new Date();
+var month = today.getMonth();
+var date = (month+1) + "-" + today.getDate() + "-" + today.getFullYear();
+showDate.innerHTML=date;
 
 
